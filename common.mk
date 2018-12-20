7dfb2288a2036b95878ef9dc418404b887c0e2cf
Kimmo Lindholm <kimmo@eke.fi>
2018-12-20 09:30:05 +0200

    make section

---
+++ common.mk
@@ -0,0 +1,10 @@
+COMMONSRC = \
+       shell/cmd_ping.c \
+       shell/cmd_send.c \
+       threads/threadkiller.c \
+       threads/auxlink.c \
+       threads/messaging.c
+
+COMMONINC = \
+       shell \
+       threads \
