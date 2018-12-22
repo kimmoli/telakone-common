fec89673ccdfd1ab84f424e0f30dd4625a28e05b
Kimmo Lindholm <kimmo@eke.fi>
2018-12-22 11:27:01 +0200

    include dm, reboot, move includeables to inc

--- common.mk
+++ common.mk
@@ -1,9 +1,9 @@
-COMMONSRC = $(COMMON)/shell/cmd_ping.c \
+COMMONSRC = $(COMMON)/shell/cmd_reboot.c \
+            $(COMMON)/shell/cmd_dm.c \
+            $(COMMON)/shell/cmd_ping.c \
             $(COMMON)/shell/cmd_send.c \
             $(COMMON)/threads/threadkiller.c \
             $(COMMON)/threads/auxlink.c \
             $(COMMON)/threads/messaging.c
 
-COMMONINC = $(COMMON)/inc \
-            $(COMMON)/shell \
-            $(COMMON)/threads
+COMMONINC = $(COMMON)/inc
