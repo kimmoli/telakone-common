fcd387a120121d1af5803d1ff1af634919ebdf99
Kimmo Lindholm <kimmo@eke.fi>
2018-12-22 15:40:40 +0200

    do not fail build if no motors

--- inc/auxmotor.h
+++ inc/auxmotor.h
@@ -4,11 +4,13 @@
 #define AUXMOTOR_EVENT_STOP    0x4000
 #define AUXMOTOR_EVENT_SET     0x8000
 
+#ifdef MOTORS
 #if (MOTORS == 1)
 #define LINE_MOTOR1_L1 LINE_MOTORL1
 #define LINE_MOTOR1_L2 LINE_MOTORL2
 #define TK_PWM_MOTOR1H1 TK_PWM_MOTORH1
 #define TK_PWM_MOTOR1H2 TK_PWM_MOTORH2
+#endif
 #endif
 
 typedef struct
