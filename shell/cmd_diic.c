9446945caf2ad53e7f053c9c83cc8fbd525adea7
Kimmo Lindholm <kimmo@eke.fi>
2019-01-12 14:05:05 +0200

    diic to support reading count of single bytes

--- shell/cmd_diic.c
+++ shell/cmd_diic.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <string.h>
 #include "hal.h"
 #include "chprintf.h"
 #include "shellcommands.h"
@@ -14,21 +15,36 @@ void cmd_diic(BaseSequentialStream *chp, int argc, char *argv[])
 
     if (argc < 2)
     {
-        chprintf(chp, "diic deviceaddress regaddress {count}\n\r");
+        chprintf(chp, "diic deviceaddress regaddress {count} {s}\n\r");
         return;
     }
 
     addr = strtol(argv[0], NULL, 16);
     regaddr = strtol(argv[1], NULL, 16);
 
-    if (argc == 3)
+    if (argc >= 3)
         count = strtol(argv[2], NULL, 10);
     else
         count = 1;
 
-    i2cAcquireBus(&I2CD1);
-    ret |= i2cMasterTransmit(&I2CD1, addr, (uint8_t[]){regaddr}, 1, rxBuf, count);
-    i2cReleaseBus(&I2CD1);
+    if ((argc >= 4) && (strcmp(argv[3], "s") == 0))
+    {
+        i2cAcquireBus(&I2CD1);
+        for (int i = 0 ; i < count ; i++)
+        {
+            ret |= i2cMasterTransmit(&I2CD1, addr, (uint8_t[]){regaddr+i}, 1, rxBuf+i, 1);
+
+            if (ret != MSG_OK)
+                break;
+        }
+        i2cReleaseBus(&I2CD1);
+    }
+    else
+    {
+        i2cAcquireBus(&I2CD1);
+        ret |= i2cMasterTransmit(&I2CD1, addr, (uint8_t[]){regaddr}, 1, rxBuf, count);
+        i2cReleaseBus(&I2CD1);
+    }
 
     if (ret != MSG_OK)
     {
